7ff8c8f177cbb0c6a0bd3e6462443eca18ddb48d
TheGostsniperfr <brianperret.pro@gmail.com>
2024-08-03 00:11:00 +0200

    ✅ Add Darktable

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -15,6 +15,8 @@
     ../../user/app/social/whatsapp/whatsapp.nix
     ../../user/app/work/microsoft/todo.nix
     ../../user/app/nodejs/nodejs.nix
+    ../../user/app/photo/darktable/darktable.nix
+
     ../../user/shell/cli-collection/cli-collection.nix
     ../../user/shell/sh/sh.nix
   ];
