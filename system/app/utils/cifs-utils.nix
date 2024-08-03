6ed126d954d075b11b96080dc2c6fe84078fe11e
TheGostsniperfr <brianperret.pro@gmail.com>
2024-08-03 11:12:19 +0200

    ✅ Add samba client link

---
+++ system/app/utils/cifs-utils.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = [
+    pkgs.cifs-utils
+  ];
+
+  fileSystems."/media/nas" = {
+    device = "";
+    fsType = "cifs";
+    options = [ "username=" "password=" ];
+  };
+}
