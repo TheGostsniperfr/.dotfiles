113cf786e9d1ae78c2cc4bb38182f2b538c4263c
brian.perret <brian.perret@epita.fr>
2025-06-05 16:24:24 +0200

    feat: Add php extensions for laravel

--- system/app/language/php/php.nix
+++ system/app/language/php/php.nix
@@ -3,12 +3,19 @@
 {
   environment.systemPackages = with pkgs; [
     php83
+    
     php83Packages.composer
-    mysql84
-  ];
 
-  services.mysql = {
-    enable = true;
-    package = pkgs.mysql84;
-  };
+    php83Extensions.pdo
+    php83Extensions.pdo_pgsql
+    php83Extensions.pgsql
+    php83Extensions.mbstring
+    php83Extensions.tokenizer
+    php83Extensions.xml
+    php83Extensions.curl
+    php83Extensions.zip
+    php83Extensions.fileinfo
+    php83Extensions.bcmath
+    php83Extensions.opcache
+  ];
 }
--- system/app/sql/postgresql.nix
+++ system/app/sql/postgresql.nix
@@ -4,9 +4,15 @@
   services.postgresql = {
     enable = true; 
     package = pkgs.postgresql_15; 
-    initialScript = pkgs.writeText "init.sql" ''
-      CREATE ROLE brian WITH LOGIN CREATEDB PASSWORD 'admin';
+    initialScript = pkgs.writeText "backend-initScript" ''
+      CREATE ROLE brian WITH LOGIN SUPERUSER PASSWORD 'admin';
       CREATE DATABASE brian OWNER brian;
+      \c brian
+      GRANT ALL PRIVILEGES ON SCHEMA public TO brian;
+
+      CREATE DATABASE arffornia;
+      CREATE USER laravel WITH ENCRYPTED PASSWORD 'laravel';
+      GRANT ALL PRIVILEGES ON DATABASE arffornia TO laravel;
     '';
   };
 }
