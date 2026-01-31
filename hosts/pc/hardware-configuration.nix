abbb2f2cf5857547f377f6f0c8081f6eaf9165a8
brian.perret <brian.perret@epita.fr>
2026-01-31 15:37:53 +0100

    feat(pc): add encryption config

--- hosts/pc/hardware-configuration.nix
+++ hosts/pc/hardware-configuration.nix
@@ -8,26 +8,29 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "rtsx_usb_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/201d7b3d-fb11-49c6-841f-b5ffa92c9ad7";
-      fsType = "ext4";
-    };
+  boot.initrd.luks.devices."luks-4569b375-83ee-458f-8fe1-cbac7f7b5c21".device = "/dev/disk/by-uuid/4569b375-83ee-458f-8fe1-cbac7f7b5c21";
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/0850-20E7";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
+  boot.initrd.luks.devices."luks-f4002469-187c-4b8d-8886-4eb0ab363ee5".device = "/dev/disk/by-uuid/f4002469-187c-4b8d-8886-4eb0ab363ee5";
 
-    swapDevices = [{
-      device = "/swapfile";
-      size = 16 * 1024; # 16GB
-    }];
+  fileSystems."/" = {
+    device = "/dev/mapper/luks-4569b375-83ee-458f-8fe1-cbac7f7b5c21";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/E359-D039";
+    fsType = "vfat";
+    options = [ "fmask=0077" "dmask=0077" ];
+  };
+
+  swapDevices = [
+    { device = "/dev/mapper/luks-f4002469-187c-4b8d-8886-4eb0ab363ee5"; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
