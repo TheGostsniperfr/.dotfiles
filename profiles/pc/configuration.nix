4bcaf40c299b74c20021ce69089e56589f242786
TheGostsniperfr <brianperret.pro@gmail.com>
2026-08-25 11:01:07 +0200

    chore(profiles): enable davinci-resolve, cisco-packet-tracer and fingerprint on pc

--- profiles/pc/configuration.nix
+++ profiles/pc/configuration.nix
@@ -12,9 +12,14 @@ in
 
       # Apps
       ../../system/app/linoffice/linoffice.nix
+      ../../system/app/video/davinci-resolve.nix
+      ../../system/app/network/cisco-packet-tracer.nix
 
       # GPU
       ../../system/hardware/nvidia/nvidia-igpu.nix
+
+      # Fingerprint reader (FocalTech 2808:a658)
+      ../../system/hardware/fingerprint/fingerprint.nix
       
       # Development tools
       # ../../system/app/language/octave/octave.nix
